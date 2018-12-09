cb6bce9b4d7c0e622586e14e58498fa7c29bd52b
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-12-08 21:17:41 -0500

    stopping point

---
+++ Database/GenealogyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Genealogy.Classes;
+
+namespace Database
+{
+    public class GenealogyContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\MSSQL;Database=Genealogy;UID=user;Password=pass;Trusted_Connection=true");
+        }
+    }
+}
