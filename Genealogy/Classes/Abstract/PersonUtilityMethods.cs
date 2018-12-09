8fd3892cfa87d11c0aba3e6c92207f66fb44f58a
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-12-08 19:30:52 -0500

    abstracting away some person-related data-retrieval logic

---
+++ Genealogy/Classes/Abstract/PersonUtilityMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Genealogy.Classes.Abstract
+{
+    public abstract class PersonUtilityMethods
+    {
+
+        protected bool CheckIfIsAlive()
+        {
+            // Whatever calculations or operations would go here.
+            // This is mainly to reduce noise in the Person class.
+            return true;
+        }
+
+        protected byte CalculateAge(int year, byte month, byte day)
+        {
+            DateTime Dob = Convert.ToDateTime($"{year}/{month}/{day}");
+            return (byte)(new DateTime(DateTime.Now.Subtract(Dob).Ticks).Year - 1);
+        }
+    }
+}
