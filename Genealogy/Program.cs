5be9b40ccac3dce4199d3f9c004d69fc3096b7a2
Juan C. Gonzalez <javascriptdeveloper17@gmail.com>
2018-12-09 13:32:40 -0500

    building Person object

--- Genealogy/Program.cs
+++ Genealogy/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Genealogy.Classes;
+using Genealogy.Classes.PersonInfo;
+using Genealogy.Enums.Person;
+using System;
+using System.Collections.Generic;
 
 namespace Genealogy
 {
@@ -6,7 +10,56 @@ namespace Genealogy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nameInfo = new NameInfo() { First = "Juan", Middle = "Carlos", Last = "Gonzalez", Maiden = "Molina" };
+            var birthInfo = new BirthInfo() { Year = 1990, Month = 11, Day = 5, Place = new BirthPlaceInfo() { City = "Guayaquil", Country = "Ecuador" } };
+            var relationsInfo = new RelationsInfo()
+            {
+                Parents = new ParentsInfo()
+                {
+                    Father = new Person()
+                    {
+                        Name = new NameInfo()
+                        {
+                            First = "Juan", Middle = "Carlos", Last = "Gonzalez", Maiden = "Almeida"
+                        },
+                        Birth = new BirthInfo()
+                        {
+                            Year = 1965, Month = 11, Day = 21,
+                            Place = new BirthPlaceInfo()
+                            {
+                                City = "Guayaquil",
+                                Country = "Ecuador"
+                            }
+                        }
+                    },
+                    Mother = new Person()
+                    {
+                        Name = new NameInfo()
+                        {
+                            First = "Sandra", Middle = "Lorena", Last = "Molina", Maiden = "Intriago"
+                        },
+                        Birth = new BirthInfo()
+                        {
+                            Year = 1966, Month = 10, Day = 22,
+                            Place = new BirthPlaceInfo()
+                            {
+                                City = "Guayaquil",
+                                Country = "Ecuador"
+                            }
+                        }
+                    }
+                },
+                Siblings =
+                {
+                    Sisters = new List<Person>()
+                    {
+                        new Person()
+                    }
+                }
+
+            };
+            Person p = new Person(nameInfo, birthInfo, Sex.M, null, relationsInfo);
+            Console.WriteLine(p.Relations.Parents.Mother.GetFullLegalName());
         }
     }
 }
